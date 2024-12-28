787895210a44fe222df1970ceee069a717134ca9
Preetham <preethamvenkatram@gmail.com>
2024-12-28 12:59:52 +0530

    added functions

--- contracts/Medical.sol
+++ contracts/Medical.sol
@@ -403,5 +403,352 @@ contract Healthcare {
 
     //--------------END OF PATIENT------------------
 
+     //--------------ADMIN------------------
 
+    //UPADTE ONLY ADMIN
+    function UPDATE_REGISTRATION_FEE(uint _newFee) public onlyAdmin {
+        registrationDoctorFee = _newFee;
+
+         ADD_NOTIFICATION(admin, "You have successfully updated Registration fee", "Admin");
+    }
+
+    function UPDATE_APPOINTMENT_FEE(uint _newFee) public onlyAdmin {
+        appointmentFee = _newFee;
+
+         ADD_NOTIFICATION(admin, "You have successfully updated Appointment fee", "Admin");
+    }
+
+    function UPDATE_REGISTRATION_PATIENT_FEE(uint _newFee) public onlyAdmin {
+        registrationPatientFee = _newFee;
+
+         ADD_NOTIFICATION(admin, "You have successfully updated Patient Registration fee", "Admin");
+    }
+
+    function UPDATE_ADMIN_ADDRESS(address payable _newAddress) public onlyAdmin {
+        admin = _newAddress;
+
+         ADD_NOTIFICATION(admin, "You have successfully updated admin address", "Admin");
+    }
+
+    //--------------END OF ADMIN------------------
+
+    
+    //--------------GET APTIENT------------------
+
+    function GET_ALL_PATIENT_ORDERS(uint patientId) public view returns (Order[] memory) {
+        require(patientId <= patientCount, "Patient does not exist");
+        return patientOrders[patientId];
+    }
+
+    //GET PRESCRIPTION DETAILS
+    function GET_PRESCRIPTION_DETAILS(uint _prescriptionId) public view returns (Prescription memory) {
+        return prescriptions[_prescriptionId];
+    }
+
+    // GET_ALL_PRESCRIBED_MEDICINES
+    
+    function GET_ALL_PRESCRIBED_MEDICINES() public view returns (Prescription[] memory) {
+        Prescription[] memory allPrescriptions = new Prescription[](prescriptionCount);
+        uint counter = 0;
+        for (uint i = 1; i <= prescriptionCount; i++) {
+            allPrescriptions[counter] = prescriptions[i];
+            counter++;
+        }
+        return allPrescriptions;
+    }
+
+    function GET_ALL_PRESCRIBED_MEDICINES_OF_PATIENT(uint patientId) public view returns (Prescription[] memory) {
+        uint count = 0;
+        for (uint i = 1; i <= prescriptionCount; i++) {
+            if (prescriptions[i].patientId == patientId) {
+                count++;
+            }
+        }
+
+        Prescription[] memory patientPrescriptions = new Prescription[](count);
+        uint counter = 0;
+        for (uint i = 1; i <= prescriptionCount; i++) {
+            if (prescriptions[i].patientId == patientId) {
+                patientPrescriptions[counter] = prescriptions[i];
+                counter++;
+            }
+        }
+        return patientPrescriptions;
+    }
+
+    function GET_ALL_REGISTERED_PATIENTS() public view returns (Patient[] memory) {
+        Patient[] memory allPatients = new Patient[](patientCount);
+        uint counter = 0;
+        for (uint i = 1; i <= patientCount; i++) {
+            allPatients[counter] = patients[i];
+            counter++;
+        }
+        return allPatients;
+    }
+
+    function GET_PATIENT_ID(address _patientAddress) public view returns (uint) {
+        for (uint i = 1; i <= patientCount; i++) {
+            if (patients[i].accountAddress == _patientAddress) {
+                return i;
+            }
+        }
+        revert("Patient not found");
+    }
+
+    function GET_PATIENT_APPOINTMENT(uint _appointmentId) public view returns (Appointment memory) {
+        require(_appointmentId <= appointmentCount, "Appointment does not exist");
+        require(appointments[_appointmentId].patientId == GET_PATIENT_ID(msg.sender) || msg.sender == admin, "Only the patient or admin can view the appointment");
+        return appointments[_appointmentId];
+    }
+
+    function GET_PATIENT_MEDICIAL_HISTORY(uint _patientId) public view returns (string[] memory) {
+        require(_patientId <= patientCount, "Patient does not exist");
+        require(patients[_patientId].accountAddress == msg.sender || msg.sender == admin, "Only the patient or admin can view the medical history");
+        return patients[_patientId].medicalHistory;
+    }
+
+    function GET_PATIENT_APPOINTMENT_HISTORYS(uint _patientId) public view returns (Appointment[] memory) {
+        require(_patientId <= patientCount, "Patient does not exist");
+
+        uint count = 0;
+        for (uint i = 1; i <= appointmentCount; i++) {
+            if (appointments[i].patientId == _patientId) {
+                count++;
+            }
+        }
+
+        Appointment[] memory patientAppointments = new Appointment[](count);
+        uint counter = 0;
+        for (uint i = 1; i <= appointmentCount; i++) {
+            if (appointments[i].patientId == _patientId) {
+                patientAppointments[counter] = appointments[i];
+                counter++;
+            }
+        }
+        return patientAppointments;
+   }
+
+    function GET_PATIENT_DETAILS(uint _patientId) public view returns (Patient memory) {
+        return patients[_patientId];
+    }
+
+    function GET_BOUGHT_MEDICINE_BY_PAITENT(uint _patientId) public view returns (uint[] memory) {
+        require(_patientId <= patientCount, "Patient does not exist");
+        return patients[_patientId].boughtMedicines;
+    }
+
+    function GET_ALL_APPOINTMENTS() public view returns (Appointment[] memory) {
+        Appointment[] memory allAppointments = new Appointment[](appointmentCount);
+        uint counter = 0;
+        for (uint i = 1; i <= appointmentCount; i++) {
+            allAppointments[counter] = appointments[i];
+            counter++;
+        }
+        return allAppointments;
+    }
+
+    //--------------END OF GET APTIENT------------------
+
+
+    //-------------- GET OF  DOCTOR------------------
+
+    //GET FUNCTION FOR DOCTORS
+    function GET_ALL_REGISTERED_DOCTORS() public view returns (Doctor[] memory) {
+        Doctor[] memory allDoctors = new Doctor[](doctorCount);
+        uint counter = 0;
+        for (uint i = 1; i <= doctorCount; i++) {
+            allDoctors[counter] = doctors[i];
+            counter++;
+        }
+        return allDoctors;
+    }
+
+    function GET_ALL_APPROVED_DOCTORS() public view returns (Doctor[] memory) {
+        uint approvedCount = 0;
+        for (uint i = 1; i <= doctorCount; i++) {
+            if (doctors[i].isApproved) {
+                approvedCount++;
+            }
+        }
+
+        Doctor[] memory approvedDoctors = new Doctor[](approvedCount);
+        uint counter = 0;
+        for (uint i = 1; i <= doctorCount; i++) {
+            if (doctors[i].isApproved) {
+                approvedDoctors[counter] = doctors[i];
+                counter++;
+            }
+        }
+        return approvedDoctors;
+    }
+
+    function GET_MOST_POPULAR_DOCTOR() public view returns (Doctor memory) {
+        Doctor memory bestDoctor;
+        uint highestScore = 0;
+        for (uint i = 1; i <= doctorCount; i++) {
+            uint score = doctors[i].appointmentCount + doctors[i].successfulTreatmentCount;
+            if (score > highestScore) {
+                highestScore = score;
+                bestDoctor = doctors[i];
+            }
+        }
+        return bestDoctor;
+    }
+
+    function GET_DOCTOR_DETAILS(uint _doctorId) public view returns (Doctor memory) {
+        return doctors[_doctorId];
+    }
+
+    function GET_DOCTOR_ID(address _doctorAddress) public view returns (uint) {
+        for (uint i = 1; i <= doctorCount; i++) {
+            if (doctors[i].accountAddress == _doctorAddress) {
+                return i;
+            }
+        }
+        revert("Doctor not found");
+    }
+
+    function GET_DOCTOR_APPOINTMENTS_HISTORYS(uint _doctorId) public view returns (Appointment[] memory) {
+        require(_doctorId <= doctorCount, "Doctor does not exist");
+
+        uint count = 0;
+        for (uint i = 1; i <= appointmentCount; i++) {
+            if (appointments[i].doctorId == _doctorId) {
+                count++;
+            }
+        }
+
+        Appointment[] memory doctorAppointments = new Appointment[](count);
+        uint counter = 0;
+        for (uint i = 1; i <= appointmentCount; i++) {
+            if (appointments[i].doctorId == _doctorId) {
+                doctorAppointments[counter] = appointments[i];
+                counter++;
+            }
+        }
+        return doctorAppointments;
+    }
+
+    //--------------END OF GET DOCTOR------------------
+
+
+    //-------------- GET MEDICINE------------------
+
+    //GET FUNCTION FOR MEDICATION
+    function GET_ALL_REGISTERED_MEDICINES() public view returns (Medicine[] memory) {
+        Medicine[] memory allMedicine = new Medicine[](medicineCount);
+        uint counter = 0;
+        for (uint i = 1; i <= medicineCount; i++) {
+            allMedicine[counter] = medicines[i];
+            counter++;
+        }
+        return allMedicine;
+    }
+
+    function GET_MEDICINE_DETAILS(uint _medicineId) public view returns (Medicine memory) {
+        return medicines[_medicineId];
+    }
+   
+   //--------------END OF GET MEDICINE------------------
+
+
+   //-------------- CHAT------------------
+
+    //CHECK USER EXIST
+    function CHECK_USER_EXISTS(address pubkey) public view returns(bool){
+        return bytes(userList[pubkey].name).length > 0;
+    }
+
+    //CREATE ACCOUNT
+    function CREATE_ACCOUNT(string calldata name, address _address, string memory _type) internal {
+        require(CHECK_USER_EXISTS(_address) == false, "User already exists");
+        require(bytes(name).length>0, "Username cannot be empty");
+
+        userList[_address].name = name;
+        userList[_address].userType = _type;
+
+        getAllUsers.push(AllUserStruck(name, _address));
+    }
+
+    //GET USERNAME
+    function GET_USERNAME_TYPE(address pubkey) external view returns(User memory){
+        require(CHECK_USER_EXISTS(pubkey), "User is not registered");
+        return userList[pubkey];
+    }
+
+     function ADD_FRIEND(address friend_key, string calldata name, address _myAddress) internal {
+        
+        require(CHECK_USER_EXISTS(_myAddress), "Create an account first");
+        require(CHECK_USER_EXISTS(friend_key), "User is not registered!");
+        require(_myAddress != friend_key, "Users cannot add themselves as friends");
+
+    
+        if (!CHECK_ALREADY_FRIENDS(_myAddress, friend_key)) {
+            _ADD_FRIEND(_myAddress, friend_key, name);
+            _ADD_FRIEND(friend_key, _myAddress, userList[_myAddress].name);
+        }
+    }
+
+    //checkAlreadyFriends
+    function CHECK_ALREADY_FRIENDS(address pubkey1, address pubkey2) internal view returns (bool){
+
+        if(userList[pubkey1].friendList.length > userList[pubkey2].friendList.length){
+            address tmp = pubkey1;
+            pubkey1 = pubkey2;
+            pubkey2 = tmp;
+        }
+
+        for(uint256 i = 0; i < userList[pubkey1].friendList.length; i++){
+            
+            if(userList[pubkey1].friendList[i].pubkey == pubkey2) return true;
+        }
+        return false;
+    }
+
+    function _ADD_FRIEND(address me, address friend_key, string memory name) internal{
+        friend memory newFriend = friend(friend_key, name);
+       userList[me].friendList.push(newFriend);
+    }
+
+    //GETMY FRIEND
+    function GET_MY_FRIEND_LIST(address _address) external view returns(friend[] memory){
+        return userList[_address].friendList;
+    }
+
+    //get chat code
+    function _GET_CHAT_CODE(address pubkey1, address pubkey2) internal pure returns(bytes32){
+        if(pubkey1 < pubkey2){
+            return keccak256(abi.encodePacked(pubkey1, pubkey2));
+        } else 
+        return keccak256(abi.encodePacked(pubkey2, pubkey1));
+    }
+
+    //SEND MESSAGE
+    function _SEND_MESSAGE(address friend_key, address _myAddress, string calldata _msg) external{
+        require(CHECK_USER_EXISTS(_myAddress), "Create an account first");
+        require(CHECK_USER_EXISTS(friend_key), "User is not registered");
+        require(CHECK_ALREADY_FRIENDS(_myAddress, friend_key), "You are not friend with the given user");
+
+        bytes32 chatCode = _GET_CHAT_CODE(_myAddress, friend_key);
+        message memory newMsg = message(_myAddress, block.timestamp, _msg);
+        allMessages[chatCode].push(newMsg);
+
+        ADD_NOTIFICATION(_myAddress, "You have successfully send message", "Message");
+
+        ADD_NOTIFICATION(friend_key, "You have new message", "Message");
+
+        ADD_NOTIFICATION(admin, "message send successfully", "Message");
+    }
+
+    //READ MESSAGE
+    function GET_READ_MESSAGE(address friend_key, address _myAddress) external view returns(message[] memory){
+        bytes32 chatCode = _GET_CHAT_CODE(_myAddress, friend_key);
+        return allMessages[chatCode];
+    }
+
+    function GET_ALL_APP_USER() public view returns(AllUserStruck[] memory){
+        return getAllUsers;
+    }
+
+     //--------------END OF CHAT------------------
 }
