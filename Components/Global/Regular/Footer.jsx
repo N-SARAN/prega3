8e2294293f1d2e08f777fcf35d955368a7683a9d
Preetham <preethamvenkatram@gmail.com>
2024-12-28 18:22:09 +0530

    added data components

---
+++ Components/Global/Regular/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        <p>
+          Copyright © Designed &amp; Developed by
+          <a href="https://www.harshakumar.vercel.app.com/" target="_blank">
+            @CryptoVenture Innovators
+          </a>
+          2024
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
