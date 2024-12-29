bb09babbe2001393d8604283b355b4bcebb5199b
ahkharsha <ahkharsha@gmail.com>
2024-12-29 10:34:07 +0530

    Fixes

--- pages/anon-aadhar/verification.js
+++ pages/anon-aadhar/verification.js
@@ -117,7 +117,7 @@ export default function AadhaarVerification({ onVerified }) {
               prega3
             </Typography>
             <Typography variant="h5" sx={{ color: "secondary.main", mb: 5 }}>
-              Your Gateway to Smart Real Estate Deals
+              Motherhood Monitored, Miracles Managed
             </Typography>
             <Typography variant="h6" color="textSecondary">
               Please connect your MetaMask wallet and verify your Aadhaar
