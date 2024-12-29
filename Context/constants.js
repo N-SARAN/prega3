c572fce0e32f9c6c49c532491b9731ec68e10863
ahkharsha <ahkharsha@gmail.com>
2024-12-29 10:20:14 +0530

    Deploy on polygon mainnet

--- Context/constants.js
+++ Context/constants.js
@@ -24,39 +24,6 @@ const PINATA_SECRECT_KEY = process.env.NEXT_PUBLIC_PINATA_SECRECT_KEY;
 
 //NETWORK
 const networks = {
-  holesky: {
-    chainId: `0x${Number(17000).toString(16)}`,
-    chainName: "Holesky",
-    nativeCurrency: {
-      name: "ETH",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://rpc.ankr.com/eth_holesky"],
-    blockExplorerUrls: ["https://holesky.etherscan.io/"],
-  },
-  sepolia: {
-    chainId: `0x${Number(11155111).toString(16)}`,
-    chainName: "Sepolia",
-    nativeCurrency: {
-      name: "SepoliaETH",
-      symbol: "SepoliaETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://sepolia.infura.io/v3/"],
-    blockExplorerUrls: ["https://sepolia.etherscan.io"],
-  },
-  polygon_amoy: {
-    chainId: `0x${Number(80002).toString(16)}`,
-    chainName: "Polygon Amoy",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
-    },
-    rpcUrls: ["https://rpc.ankr.com/polygon_amoy"],
-    blockExplorerUrls: ["https://www.oklink.com/amoy"],
-  },
   polygon: {
     chainId: `0x${Number(137).toString(16)}`,
     chainName: "Polygon Mainnet",
@@ -67,39 +34,6 @@ const networks = {
     },
     rpcUrls: ["https://rpc.ankr.com/polygon"],
     blockExplorerUrls: ["https://polygonscan.com/"],
-  },
-  bsc: {
-    chainId: `0x${Number(56).toString(16)}`,
-    chainName: "Binance Smart Chain Mainnet",
-    nativeCurrency: {
-      name: "Binance Chain Native Token",
-      symbol: "BNB",
-      decimals: 18,
-    },
-    rpcUrls: ["https://rpc.ankr.com/bsc"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  base_mainnet: {
-    chainId: `0x${Number(8453).toString(16)}`,
-    chainName: "Base Mainnet",
-    nativeCurrency: {
-      name: "ETH",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://mainnet.base.org/"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  base_sepolia: {
-    chainId: `0x${Number(84532).toString(16)}`,
-    chainName: "Base Sepolia",
-    nativeCurrency: {
-      name: "ETH",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://sepolia.base.org"],
-    blockExplorerUrls: ["https://bscscan.com"],
   },
   localhost: {
     chainId: `0x${Number(31337).toString(16)}`,
